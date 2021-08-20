70c7a8b8b26ec89987af2fee44a696899814f31a
Sergei <igrofay@gmail.com>
2021-08-20 13:33:31 +0500

    1.0.0

---
+++ app/src/main/java/food/novgorod/legends/MainActivity.kt
@@ -0,0 +1,14 @@
+package food.novgorod.legends
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import food.novgorod.legends.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/food/novgorod/legends/ui/welcome/SignUpFragment.kt
@@ -0,0 +1,20 @@
+package food.novgorod.legends.ui.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import food.novgorod.legends.R
+class SignUpFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+}
