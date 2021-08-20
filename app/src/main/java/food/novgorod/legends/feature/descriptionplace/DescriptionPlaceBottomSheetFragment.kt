81a42fc04fa9a4e71fa222eafdc020d3b70d4a7d
Vladislav <59665801+ArsenalNox@users.noreply.github.com>
2021-08-21 04:53:59 +0500

    KFka

---
+++ app/src/main/java/food/novgorod/legends/feature/descriptionplace/DescriptionPlaceBottomSheetFragment.kt
@@ -0,0 +1,21 @@
+package food.novgorod.legends.feature.descriptionplace
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import food.novgorod.legends.databinding.FragmentDescriptionPlaceBinding
+
+class DescriptionPlaceBottomSheetFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentDescriptionPlaceBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentDescriptionPlaceBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+}
