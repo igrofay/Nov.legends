d6fbdde1d403a8a00d776b8cb58c6141f5846946
Vladislav <59665801+ArsenalNox@users.noreply.github.com>
2021-08-20 19:51:43 +0500

    1.0.2

---
+++ app/src/main/java/food/novgorod/legends/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package food.novgorod.legends
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import food.novgorod.legends.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+    lateinit var binding : ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/food/novgorod/legends/ui/welcome/AnnotationFragment.kt
+++ app/src/main/java/food/novgorod/legends/ui/welcome/AnnotationFragment.kt
@@ -5,17 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import food.novgorod.legends.R
+import food.novgorod.legends.databinding.FragmentAnnotationBinding
 
 
 class AnnotationFragment : Fragment() {
-
+    private var _binding: FragmentAnnotationBinding?=null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_annotation, container, false)
+        _binding = FragmentAnnotationBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageNext.setOnClickListener{
+            NavHostFragment.findNavController(this).navigate(R.id.action_annotationFragment_to_signUpFragment)
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
