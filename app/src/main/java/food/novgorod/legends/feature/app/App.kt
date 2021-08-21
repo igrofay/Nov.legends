5bb7f82831d7e3c3c909ba158396cab85e1607bf
Vladislav <59665801+ArsenalNox@users.noreply.github.com>
2021-08-22 04:36:59 +0500

    d

--- app/src/main/java/food/novgorod/legends/feature/app/App.kt
+++ app/src/main/java/food/novgorod/legends/feature/app/App.kt
@@ -2,15 +2,28 @@ package food.novgorod.legends.feature.app
 
 import android.app.Application
 import android.content.Context
+import android.content.SharedPreferences
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 
 class App: Application() {
+
     companion object {
+        const val prefTag = "AppData"
+        const val DARK = "isDark"
         lateinit var appContext: Context
     }
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+        setTheme()
     }
+    private fun setTheme(){
+        val isDark = applicationContext
+            .getSharedPreferences( prefTag  , Context.MODE_PRIVATE).getBoolean(DARK , false)
+        AppCompatDelegate.setDefaultNightMode(
+            if(isDark) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
 
+    }
 }
