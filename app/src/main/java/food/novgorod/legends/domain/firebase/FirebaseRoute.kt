c909d6a267f9ce3cdae984b9d3f566bc964a1e38
Igor Starkov <Igor.Starkov@avalab.io>
2021-08-21 17:44:33 +0500

    WIP

--- app/src/main/java/food/novgorod/legends/domain/firebase/FirebaseRoute.kt
+++ app/src/main/java/food/novgorod/legends/domain/firebase/FirebaseRoute.kt
@@ -1,6 +1,14 @@
 package food.novgorod.legends.domain.firebase
 
+import food.novgorod.legends.data.models.Feedback
+
 sealed class FirebaseRoute {
     object UsersRoute: FirebaseRoute()
     data class UserRoute(val nickname: String): FirebaseRoute()
+    data class FeedbacksOfPlace(val placeId: String): FirebaseRoute()
+    data class CreateFeedback(val feedback: Feedback): FirebaseRoute()
+    object Places: FirebaseRoute()
+    data class Place(val placeId: String): FirebaseRoute(
+
+    )
 }
