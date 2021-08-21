5aec04f3d6ed505e51a1ad529a8de58fd2baddea
Vladislav <59665801+ArsenalNox@users.noreply.github.com>
2021-08-21 07:46:54 +0500

    KFka

--- app/src/main/java/food/novgorod/legends/feature/descriptionplace/DescriptionPlaceBottomSheetFragment.kt
+++ app/src/main/java/food/novgorod/legends/feature/descriptionplace/DescriptionPlaceBottomSheetFragment.kt
@@ -1,9 +1,11 @@
 package food.novgorod.legends.feature.descriptionplace
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import food.novgorod.legends.R
 import food.novgorod.legends.databinding.FragmentDescriptionPlaceBinding
@@ -13,8 +15,10 @@ import food.novgorod.legends.databinding.FragmentDescriptionPlaceBinding
 
 
 class DescriptionPlaceBottomSheetFragment : BottomSheetDialogFragment() {
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog = BottomSheetDialog(requireContext(), theme)
 
-    private lateinit var binding: FragmentDescriptionPlaceBinding
+private lateinit var binding: FragmentDescriptionPlaceBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
