0e503395334b053a2df4eeef8388710718ffdde7
Vladislav <59665801+ArsenalNox@users.noreply.github.com>
2021-08-22 09:17:36 +0500

    y

--- app/src/main/java/food/novgorod/legends/feature/main/BottomSheetFilterFragment.kt
+++ app/src/main/java/food/novgorod/legends/feature/main/BottomSheetFilterFragment.kt
@@ -1,10 +1,13 @@
 package food.novgorod.legends.feature.main
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import food.novgorod.legends.R
 import food.novgorod.legends.databinding.FragmentFilterBinding
 
 class BottomSheetFilterFragment : BottomSheetDialogFragment(){
@@ -13,8 +16,27 @@ class BottomSheetFilterFragment : BottomSheetDialogFragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+        val  viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         binding = FragmentFilterBinding.inflate(inflater, container, false)
+        binding.checkBox1.isChecked = viewModel.hashMap[R.string.legends] == true
+        binding.checkBox2.isChecked = viewModel.hashMap[R.string.tradition] == true
+        binding.checkBox3.isChecked = viewModel.hashMap[R.string.fairy_tales] == true
+        binding.checkBox4.isChecked = viewModel.hashMap[R.string.lake] == true
+        binding.checkBox5.isChecked = viewModel.hashMap[R.string.source] == true
+        binding.checkBox6.isChecked = viewModel.hashMap[R.string.stone] == true
+        binding.checkBox7.isChecked = viewModel.hashMap[R.string.other] == true
         return binding.root
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        val  viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        viewModel.hashMap[R.string.legends] = binding.checkBox1.isChecked
+        viewModel.hashMap[R.string.tradition] =binding.checkBox2.isChecked
+        viewModel.hashMap[R.string.fairy_tales] =binding.checkBox3.isChecked
+        viewModel.hashMap[R.string.lake] = binding.checkBox4.isChecked
+        viewModel.hashMap[R.string.source] = binding.checkBox5.isChecked
+        viewModel.hashMap[R.string.stone] = binding.checkBox6.isChecked
+        viewModel.hashMap[R.string.other] = binding.checkBox7.isChecked
     }
 }
--- app/src/main/java/food/novgorod/legends/feature/main/MainViewModel.kt
+++ app/src/main/java/food/novgorod/legends/feature/main/MainViewModel.kt
@@ -1,6 +1,16 @@
 package food.novgorod.legends.feature.main
 
 import androidx.lifecycle.ViewModel
-class MainViewModel: ViewModel() {
+import food.novgorod.legends.R
 
+class MainViewModel: ViewModel() {
+    val hashMap = mutableMapOf(
+        R.string.legends to true ,
+        R.string.tradition to true ,
+        R.string.fairy_tales to true  ,
+        R.string.lake to true ,
+        R.string.source to true ,
+        R.string.stone to true ,
+        R.string.other to true
+    )
 }
